97a69ad311251a55f1658f0904b5f80bc210f0c6
Max Anderson <manderso@thoughtworks.com>
2017-03-08 21:23:41 -0800

    first pass at writing hop file. not the most beautiful

--- hop/cli/init_command.py
+++ hop/cli/init_command.py
@@ -1,5 +1,17 @@
 from pathlib import Path
 
+#TODO extract this from being constant string when it gets more complicated
+BASE_HOP_CONFIG = '''name: hop-gocd
+provider:
+    name: local_docker
+    agents:
+        instances: 2
+'''
+
+def generate_hop_config(hop_file):
+    hop_file.touch()
+    hop_file.write_text(BASE_HOP_CONFIG)
+
 class InitCommand(object):
     def __init__(self, args, ):
         self.args = args
@@ -8,4 +20,4 @@ class InitCommand(object):
         hop_dir = Path(self.args.dest_dir)
         hop_dir.mkdir(parents=True)
         hop_config = hop_dir / 'hop.yml'
-        hop_config.touch()
+        generate_hop_config(hop_config)
--- test/testintegration/cli/test_init_command.py
+++ test/testintegration/cli/test_init_command.py
@@ -3,19 +3,29 @@ from pathlib import Path
 from hop.cli.init_command import InitCommand
 from hop import create_parser
 
-test_dir_path = '/tmp/testinitcommand'
+TEST_DIR_PATH = '/tmp/testinitcommand'
+EXPECTED_CONTENTS = '''name: hop-gocd
+provider:
+    name: local_docker
+    agents:
+        instances: 2
+'''
 
 class TestInitCommand(unittest.TestCase):
     def setUp(self):
         self.parser = create_parser()
 
     def tearDown(self):
-        test_dir = Path(test_dir_path)
+        test_dir = Path(TEST_DIR_PATH)
         (test_dir / 'hop.yml').unlink() #you can't delete nonempty dirs in python
         test_dir.rmdir()
 
     def test_should_create_hop_config_in_specified_folder(self):
-        InitCommand(self.parser.parse_args(['init', test_dir_path])).execute()
-        config = Path(test_dir_path + '/hop.yml')
+
+        InitCommand(self.parser.parse_args(['init', TEST_DIR_PATH])).execute()
+
+        config = Path(TEST_DIR_PATH + '/hop.yml')
         self.assertTrue(config.is_file(), 'file appears not to exist')
-        #do something here to make sure the file looks right
+
+        config_contents = config.read_text()
+        self.assertEqual(config_contents, EXPECTED_CONTENTS, "hop.yml contents did not match expected contents")
